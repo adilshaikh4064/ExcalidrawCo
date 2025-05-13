493c163ccd3612a0ac0834aeb435c4142a1c251a
adil <adil.business4064@gmail.com>
2025-05-13 23:56:34 +0530

    before frontend

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,6 +1,6 @@
 const JWT_SECRET=process.env.JWT_SECRET || 'a-random-jwt-secret';
 const SALT_ROUNDS=Number(process.env.SALT_ROUNDS) || 10;
-const HTTP_PORT=Number(process.env.HTTP_PORT) || 3000;
+const HTTP_PORT=Number(process.env.HTTP_PORT) || 3001;
 
 export const config={
     JWT_SECRET,
