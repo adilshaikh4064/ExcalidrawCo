e2d5ca2cc4f72209d983919305c7a6e03b9b4a41
adil <adil.business4064@gmail.com>
2025-05-15 21:33:04 +0530

    going to set up tailwind

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,15 @@
-
+import {ThemeButton} from '@repo/ui/ThemeButton';
 
 export default function Home() {
-  return (
-    <div className="bg-red-400">hello world</div>
-  );
+    return (
+        <div className="min-h-screen bg-white min-w-screen ">
+            <div className="w-40 h-40 bg-blue-300 dark:bg-green-300">
+                hello world
+            </div>
+            <br/>
+            <div>
+                <ThemeButton/>
+            </div>
+        </div>
+    );
 }
